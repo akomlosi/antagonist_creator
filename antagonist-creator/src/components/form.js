a25353290588e7d3fe81409d64f0675e24969c16
Attila Komlosi <akomlosi@users.noreply.github.com>
2025-04-12 23:05:47 +0200

    done

---
+++ antagonist-creator/src/components/form.js
@@ -0,0 +1,163 @@
+import { config } from "../config/config";
+import { generate } from "../utils/antagonist-generator";
+
+const Hex = ({ num, className, children }) => (
+  <div className={`hex-wrapper ${className}`}>
+    <span className="title">{children}</span>
+    <svg viewBox="0 0 100 95" class="hex-svg">
+      <polygon
+        points="50,0 100,25 100,75 50,100 0,75 0,25"
+        fill="none"
+        stroke="#59b3b7"
+        stroke-width="3"
+      />
+      <text x="50%" y="58%" dominant-baseline="middle" text-anchor="middle">
+        {num}
+      </text>
+    </svg>
+  </div>
+);
+
+const BracketedBox = ({ title, children }) => (
+  <>
+    <div className="chevron-down">{title}</div>
+    <div>
+      <div className="bracketed-box">{children}</div>
+    </div>
+  </>
+);
+
+export const Form = () => {
+  const { antagonists } = config;
+  const critter = generate(antagonists);
+  return (
+    <article>
+      <div className="div1 text_heading text_highlighted">
+        {critter.difficulty.description}
+        {critter.difficulty.attack_num === 2 && (
+          <div className="danger text_md">
+            [Attacks <span className="text_highlighted">2</span> times on its
+            turn]
+          </div>
+        )}
+      </div>
+      <div className="top_block">
+        <div className="hex_block">
+          <Hex num={critter.difficulty.STR} className={"div1"}>
+            STR
+          </Hex>
+          <Hex num={critter.difficulty.DEX} className={"div2"}>
+            DEX
+          </Hex>
+          <Hex num={critter.difficulty.CON} className={"div3"}>
+            CON
+          </Hex>
+          <Hex num={critter.difficulty.WIL} className={"div4"}>
+            WIL
+          </Hex>
+          <Hex num={critter.difficulty.INT} className={"div5"}>
+            INT
+          </Hex>
+          <Hex num={critter.difficulty.CHA} className={"div6"}>
+            CHA
+          </Hex>
+        </div>
+        <div className="stats">
+          <div>
+            <span className="chevron-right">wounds</span>
+            <span className=" text_highlighted boxed-stat">
+              {critter.difficulty.wounds}
+            </span>
+          </div>
+          <div>
+            <span className="chevron-right">awareness</span>
+            <span className=" text_highlighted boxed-stat">
+              {critter.difficulty.awareness}
+            </span>
+          </div>
+          <div>
+            <span className="chevron-right">armor</span>
+            <span className=" text_highlighted boxed-stat">
+              {critter.difficulty.armor === 0 ? "-" : critter.difficulty.armor}
+            </span>
+          </div>
+          <div>
+            <span className="chevron-right">defense</span>
+            <span className="div8 text_highlighted boxed-stat">
+              {critter.difficulty.defense === 0
+                ? "-"
+                : critter.difficulty.defense}
+            </span>
+          </div>
+        </div>
+      </div>
+      <BracketedBox title={"Damage"}>
+        <div>
+          <span className="chevron-right">Attack skill</span>
+          <span className="text_highlighted">
+            +{critter.difficulty.damage.attack_skill}
+          </span>
+        </div>
+        <div>
+          <span className="chevron-right">Damage</span>
+          <span className="text_highlighted">
+            +{critter.difficulty.damage.damage_mod}
+          </span>
+        </div>
+      </BracketedBox>
+      <div>
+        <span className="chevron-right"> Range (if applicable)</span>
+        <span className="text_highlighted">{critter.difficulty.range}</span>
+      </div>
+      <div>
+        <span className="chevron-right">abilities</span>
+        <span className="danger">{critter.ability}</span>
+      </div>
+      {critter.difficulty.recovery && (
+        <div>
+          <div className="chevron-down">Recovery reactions</div>
+          <div className="space-left">
+            <div>
+              <span className="danger space-right angle-right">[Bloodied]</span>
+              <span className="neutral">
+                {critter.difficulty.recovery.bloodied}
+              </span>
+            </div>
+            <div>
+              <span className="danger space-right angle-right">[Cornered]</span>
+              <span className="neutral">
+                {critter.difficulty.recovery.cornered}
+              </span>
+            </div>
+            <div>
+              <span className="danger space-right angle-right">
+                [Overwhelmed]
+              </span>
+              <span className="neutral">
+                {critter.difficulty.recovery.overwhelmed}
+              </span>
+            </div>
+          </div>
+        </div>
+      )}
+      <div>
+        <span className="chevron-right">Appearance</span>
+        <span className="neutral">{critter.appearance}</span>
+      </div>
+      <div>
+        <span className="chevron-right">Type</span>
+        <span className="neutral">{critter.type}</span>
+      </div>
+      <BracketedBox title={<div>{critter.role}</div>}>
+        {critter.role_properties.map((p) => (
+          <span className="angle-right">{p}</span>
+        ))}
+        <span className="angle-right">{critter.role_properties}</span>
+      </BracketedBox>
+      <div>
+        <span className="chevron-right">Size</span>
+        <span className="neutral">{critter.size}</span>
+      </div>
+    </article>
+  );
+};
---
+++ antagonist-creator/src/utils/antagonist-generator.js
@@ -0,0 +1,138 @@
+function rand(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error(
+      `A rand függvény csak tömböt fogad el. ${typeof arr} ${JSON.stringify(
+        arr
+      )}`
+    );
+  }
+
+  if (arr.length === 0) {
+    throw new Error("A tömb üres, nem lehet elemet választani.");
+  }
+
+  if (arr.length === 1) {
+    return arr[0];
+  }
+
+  const index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
+
+const generateDiceRoll = (diceRoll) => {
+  const simplePattern = /^(\d+)?([+-])?\(?(\d+)D(\d+)\)?$/i;
+
+  const match = diceRoll.match(simplePattern);
+  if (!match) {
+    throw new Error(`Hibás dobás formátum: "${diceRoll}"`);
+  }
+
+  const baseValue = parseInt(match[1], 10) || 0;
+  const operator = match[2] || null;
+  const numberOfDice = parseInt(match[3], 10);
+  const diceSides = parseInt(match[4], 10);
+
+  if (![4, 6, 8, 10, 12, 20, 100].includes(diceSides)) {
+    throw new Error(`not supported format: ${diceRoll}`);
+  }
+
+  let rollTotal = 0;
+  for (let i = 0; i < numberOfDice; i++) {
+    rollTotal += Math.floor(Math.random() * diceSides) + 1;
+  }
+
+  if (operator === "+") return baseValue + rollTotal;
+  if (operator === "-") return baseValue - rollTotal;
+  return rollTotal;
+};
+
+const mergeSkills = (modifiers, target) => {
+  const result = structuredClone(target);
+
+  for (const key in modifiers) {
+    if (!target.hasOwnProperty(key)) continue;
+
+    const modValue = modifiers[key];
+    const targetValue = target[key];
+    let valueToAdd;
+
+    if (typeof modValue === "string" && modValue.includes("D")) {
+      valueToAdd = generateDiceRoll(modValue);
+    }
+
+    if (typeof modValue === "number") {
+      let valueToAdd = modValue;
+
+      if (typeof targetValue === "number") {
+        result[key] += valueToAdd;
+      }
+    } else if (Array.isArray(modValue)) {
+      if (Array.isArray(targetValue)) {
+        result[key] = [...targetValue, ...modValue];
+      } else {
+        result[key] = modValue;
+      }
+    } else if (typeof modValue === "object" && modValue !== null) {
+      result[key] = mergeSkills(modValue, targetValue);
+    }
+  }
+
+  return result;
+};
+
+export const generate = (antagonists) => {
+  const generatedConfig = {};
+  const randomDifficulty = rand(antagonists.difficulty);
+  generatedConfig.difficulty = { ...randomDifficulty };
+
+  const { damage, recovery, range } = randomDifficulty;
+
+  generatedConfig.difficulty.damage = rand(damage);
+
+  // When armor and defense is defined we randomly choose one and drop the other
+  if (generatedConfig.difficulty.armor > 0) {
+    if (Math.random() < 0.5) {
+      generatedConfig.difficulty.armor = 0;
+    } else {
+      generatedConfig.difficulty.defense = 0;
+    }
+  }
+
+  if (recovery) {
+    const rec = { ...recovery };
+    generatedConfig.difficulty.recovery = {};
+    generatedConfig.difficulty.recovery.bloodied = rand(rec.bloodied);
+    generatedConfig.difficulty.recovery.cornered = rand(rec.cornered);
+    generatedConfig.difficulty.recovery.overwhelmed = rand(rec.overwhelmed);
+  }
+
+  const mods = antagonists.mods;
+
+  const { types, sizes, intelligence, role } = mods;
+
+  generatedConfig.type = rand(types);
+
+  const randomSize = rand(sizes);
+  generatedConfig.size = randomSize.description;
+  generatedConfig.difficulty.damage.damage_mod += randomSize.damage.damage_mod;
+
+  const randomIntelligence = rand(intelligence);
+  generatedConfig.difficulty.INT = generateDiceRoll(randomIntelligence.INT);
+
+  generatedConfig.difficulty.range = generateDiceRoll(range);
+
+  const randomRole = rand(role);
+  generatedConfig.role = randomRole.description;
+  generatedConfig.role_properties = randomRole.misc;
+
+  generatedConfig.ability = rand(antagonists.abilities);
+  generatedConfig.appearance = rand(antagonists.appearance);
+
+  generatedConfig.difficulty = mergeSkills(
+    randomRole,
+    generatedConfig.difficulty
+  );
+
+  console.log(generatedConfig);
+  return generatedConfig;
+};
