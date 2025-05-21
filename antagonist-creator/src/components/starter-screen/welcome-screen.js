ba340b947eeb4895797f8325f7947a870887ec45
Attila Komlosi <akomlosi@users.noreply.github.com>
2025-05-21 20:56:39 +0200

    typo fixes and rephrased welcome text

--- antagonist-creator/src/components/starter-screen/welcome-screen.js
+++ antagonist-creator/src/components/starter-screen/welcome-screen.js
@@ -1,14 +1,25 @@
-import style from './welcome-screen.module.css';
+import style from "./welcome-screen.module.css";
 
 export const WelcomeScreen = () => {
-
   return (
     <div className={style.welcomeScreen}>
-      <h1>Welcome to the Antagonist Creator for <strong>Across A Thousand Dead Worlds</strong> game.</h1>
+      <h1>
+        Welcome to the Antagonist Creator for{" "}
+        <strong>Across A Thousand Dead Worlds</strong> game.
+      </h1>
       <p>
-        <p>This tool helps you create unique and interesting antagonists for Across A Thousan Dead Worlds game.</p>
-        <p>Either create a unique encounter or generate a list of guardians to use in your game.</p>
-        <p>To get started click on the button of your choice in the header and enjoy the game!</p>
+        <p>
+          This tool helps you create unique and interesting antagonists for
+          Across A Thousand Dead Worlds game.
+        </p>
+        <p>
+          Create a unique encounter or generate a list of guardians and use it
+          in your game.
+        </p>
+        <p>
+          To get started select a generator of your choice in the header menu
+          and enjoy the game!
+        </p>
       </p>
     </div>
   );
